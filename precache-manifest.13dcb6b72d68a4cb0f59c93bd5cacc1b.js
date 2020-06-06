26deea9495ff6fcfc013c941d8fca9092ae99f97
Stacey <scajaboleh@gmail.com>
2020-06-06 21:19:13 +0800

    Updates

---
+++ precache-manifest.13dcb6b72d68a4cb0f59c93bd5cacc1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d9d009763c7e1f0099099f18d5f7bf",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "98196112418665d19726",
+    "url": "/todoapp/static/css/main.b9c26e44.chunk.css"
+  },
+  {
+    "revision": "d5f40d4fbc50942ed993",
+    "url": "/todoapp/static/js/2.71fc73a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todoapp/static/js/2.71fc73a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98196112418665d19726",
+    "url": "/todoapp/static/js/main.0fc1c5b1.chunk.js"
+  },
+  {
+    "revision": "36c235dda1dfa6284b3d",
+    "url": "/todoapp/static/js/runtime-main.b28edc12.js"
+  }
+]);
